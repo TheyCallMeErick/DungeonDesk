919e80aefabc26e9486832eec3ad62366820e487
Azevedo-Erick <azevedoerick762@gmail.com>
2025-07-12 06:28:04 -0300

    🎨: remove blank spaces

--- DungeonDeskBackend/DungeonDeskBackend.Domain/Validations/DTOs/OnlyTheDeskMasterShoudAddOrUpdateChroniclesValidatorDTO.cs
+++ DungeonDeskBackend/DungeonDeskBackend.Domain/Validations/DTOs/OnlyTheDeskMasterShoudAddOrUpdateChroniclesValidatorDTO.cs
@@ -6,5 +6,4 @@ public record OnlyTheDeskMasterShoudAddOrUpdateChroniclesValidatorDTO
 {
     public required Desk Desk { get; set; }
     public required Guid PlayerTryingUpdateId { get; set; }
-
 }

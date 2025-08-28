799671ab5e93fe4a7fce79c35d7851bf69ce53fd
Azevedo-Erick <azevedoerick762@gmail.com>
2025-08-28 02:23:55 -0300

    ➕: add primevue to project

--- DungeonDeskFrontend/src/app/app.config.ts
+++ DungeonDeskFrontend/src/app/app.config.ts
@@ -1,12 +1,29 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-
+import { providePrimeNG } from 'primeng/config';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import Aura from '@primeuix/themes/aura';
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+        options: {
+          prefix: 'p',
+          darkModeSelector: 'system',
+          cssLayer: false,
+        },
+      },
+    }),
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+  ],
 };
--- DungeonDeskFrontend/src/app/app.routes.ts
+++ DungeonDeskFrontend/src/app/app.routes.ts
@@ -1,3 +1,31 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'desks',
+    pathMatch: 'full',
+  },
+  {
+    path: 'auth',
+    loadComponent: () => import('./layouts/auth/auth').then((m) => m.Auth),
+    children: [
+      {
+        path: 'login',
+        loadComponent: () => import('./features/auth/login/login').then((m) => m.Login),
+        //outlet: 'auth',
+      },
+      {
+        path: 'register',
+        loadComponent: () => import('./features/auth/register/register').then((m) => m.Register),
+        //outlet: 'auth',
+      },
+      {
+        path: 'forgot-password',
+        loadComponent: () =>
+          import('./features/auth/forgot-password/forgot-password').then((m) => m.ForgotPassword),
+        //outlet: 'auth',
+      },
+    ],
+  },
+];

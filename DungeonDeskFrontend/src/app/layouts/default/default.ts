0f22e0fd1d8b24051b2bd6bd227354f0775aac11
Azevedo-Erick <azevedoerick762@gmail.com>
2025-09-02 02:47:45 -0300

    ✨: create default template

---
+++ DungeonDeskFrontend/src/app/layouts/default/default.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-default',
+  imports: [RouterOutlet],
+  templateUrl: './default.html',
+  styleUrl: './default.css',
+})
+export class Default {}

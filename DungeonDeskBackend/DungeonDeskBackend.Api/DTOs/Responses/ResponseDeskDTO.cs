bffb193423994764b602ce08b21facc942fe2be5
Azevedo-Erick <azevedoerick762@gmail.com>
2025-09-02 00:41:18 -0300

    ✨: add json bindings

--- DungeonDeskBackend/DungeonDeskBackend.Api/DTOs/Responses/ResponseDeskDTO.cs
+++ DungeonDeskBackend/DungeonDeskBackend.Api/DTOs/Responses/ResponseDeskDTO.cs
@@ -1,10 +1,18 @@
+using System.Text.Json.Serialization;
+
 namespace DungeonDeskBackend.Api.DTOs.Responses; 
 
 public record ResponseDeskDTO(
+    [property: JsonPropertyName("id")]
     Guid Id,
+    [property: JsonPropertyName("name")]
     string Name,
+    [property: JsonPropertyName("description")]
     string Description,
+    [property: JsonPropertyName("status")]
     string Status,
+    [property: JsonPropertyName("max_players")]
     int MaxPlayers,
+    [property: JsonPropertyName("adventure_id")]
     Guid? AdventureId
 );

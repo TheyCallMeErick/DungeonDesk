b01614359890962b4236123b69835f031196db39
Azevedo-Erick <azevedoerick762@gmail.com>
2025-09-02 00:40:56 -0300

    ✨: add jwt authentication scheme as default

--- DungeonDeskBackend/DungeonDeskBackend.Api/Extensions/ApiServiceConfiguration.cs
+++ DungeonDeskBackend/DungeonDeskBackend.Api/Extensions/ApiServiceConfiguration.cs
@@ -1,18 +1,37 @@
+using System.Text;
 using System.Text.Json.Serialization;
 using DungeonDeskBackend.Api.Middlewares;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 using Scalar.AspNetCore;
 
 namespace DungeonDeskBackend.Api.Extensions;
 
 public static class ApiServiceConfiguration
 {
-    public static void ConfigureApiServices(this IServiceCollection services)
+    public static void ConfigureApiServices(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddControllers().AddJsonOptions(options =>
         {
             options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
         });
         services.AddOpenApi();
+        services.AddAuthentication(x=>
+        {
+            x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        }).AddJwtBearer(x =>
+    {
+        x.RequireHttpsMetadata = false;
+        x.SaveToken = true;
+        x.TokenValidationParameters = new TokenValidationParameters
+        {
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(configuration.GetValue<string>("JWT_KEY") ?? throw new Exception("The JWT_KEY is null"))),
+            ValidateIssuer = false,
+            ValidateAudience = false
+        };
+    });
+        services.AddAuthorization();
         services.AddCors(options =>
         {
             options.AddPolicy("AllowAllOrigins",
@@ -31,7 +50,8 @@ public static class ApiServiceConfiguration
             app.MapOpenApi();
             app.MapScalarApiReference();
         }
-
+        app.UseAuthentication();
+        app.UseAuthorization();
         app.UseHttpsRedirection();
         app.UseCors(builder =>
             {
--- DungeonDeskBackend/DungeonDeskBackend.Api/Program.cs
+++ DungeonDeskBackend/DungeonDeskBackend.Api/Program.cs
@@ -1,7 +1,7 @@
 using DungeonDeskBackend.Api.Extensions;
 using DungeonDeskBackend.Application.Extensions;
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.ConfigureApiServices();
+builder.Services.ConfigureApiServices(builder.Configuration);
 builder.Services.ConfigureApplicationServices(builder.Configuration);
 var app = builder.Build();
 app.UseApiServices();
